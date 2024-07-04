62bcd3cee39a0353f679719f9f7f036a26d4824a
carobte <caro.bustamante.escobar@gmail.com>
2024-07-04 07:18:14 -0500

    feat: ejercicios ciclos

---
+++ 03_ejercicios/Program.cs
@@ -0,0 +1,16 @@
+﻿// Ejercicios.
+
+// 1. Escribe un programa que imprima los números del 1 al 100 usando un ciclo for.
+
+
+
+// 2. Escribe un programa que solicite al usuario un número entero positivo e imprima todos los números pares desde 1 hasta ese número usando un ciclo for.
+// 3. Escribe un programa que pida al usuario un número entero positivo e imprima la tabla de multiplicar de ese número del 1 al 10 usando un ciclo for.
+// 4. Escribe un programa que genere un número aleatorio entre 1 y 100. Pídele al usuario que adivine el número. Muestra un mensaje indicando si el número ingresado es demasiado alto, demasiado bajo o correcto. Continúa pidiendo al usuario que adivine hasta que adivine correctamente usando un ciclo do-while.
+// 5. Escribe un programa que solicite al usuario un número entero positivo e imprima todos los números impares desde ese número hasta 1 usando un ciclo while.
+// 6. Escribe un programa que solicite al usuario una contraseña. Si la contraseña ingresada no es "secreta", sigue pidiendo al usuario que ingrese la contraseña hasta que sea correcta usando un ciclo while.
+// 7. Escribe un programa que solicite al usuario un número entero positivo e imprima la secuencia de Fibonacci hasta ese número usando un ciclo for.
+// 8. Escribe un programa que solicite al usuario un número entero positivo e imprima si el número es primo o no usando un ciclo for.
+// 9. Escribe un programa que solicite al usuario una palabra y luego imprima cada letra de la palabra en una línea separada usando un ciclo foreach.
+// 10. Escribe un programa que solicite al usuario un número entero positivo e imprima la suma de todos los números desde 1 hasta ese número usando un ciclo for.
+
