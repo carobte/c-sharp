63ff99d3dc27feb682d4a85f0474458b4ae9ab04
carobte <caro.bustamante.escobar@gmail.com>
2024-07-12 06:55:30 -0500

    fix: estructura clase venta

--- Entregables_LINQ/Tienda/Program.cs
+++ Entregables_LINQ/Tienda/Program.cs
@@ -2,19 +2,14 @@
 
 void CrearVenta()
 {
-    var listaProductos = new List<Producto>();
-
     var id = Guid.NewGuid();
     var fecha = new DateTime();
 
-    var productoId = Guid.NewGuid();
-    Console.WriteLine("Ingresa el  del producto: ");
-    var nombreProducto = Console.ReadLine();
+    Console.WriteLine("Ingresa el nombre del producto: ");
+    var nombre = Console.ReadLine();
+
     Console.WriteLine("Ingresa el valor del producto: ");
-    var valorProducto = Convert.ToDouble(Console.ReadLine());
-    
-    var producto = new Producto(productoId, nombreProducto, valorProducto
-    );
+    var valor = Convert.ToDouble(Console.ReadLine());
 
     Console.WriteLine("Ingresa la cantidad del producto: ");
     var cantidad = Convert.ToInt32(Console.ReadLine());
@@ -28,32 +23,36 @@ void CrearVenta()
     Console.WriteLine("Ingresa el tiempo de garantía: ");
     var garantia = Console.ReadLine();
 
-    listaVentas.Add(new Venta(id, fecha, producto, cantidad, vendedor, comprador, garantia));
 }
 
 public class Venta
 {
     Guid Id { set; get; }
     DateTime FechaDeVenta { set; get; }
-
-    Producto Producto { set; get; }
+    string NombreProducto { set; get; }
+    double ValorProducto { set; get; }
     int CantidadProducto { set; get; }
     string Vendedor { set; get; }
     string Comprador { set; get; }
     string TiempoGarantia { set; get; }
 
-    public Venta(Guid id,
-    DateTime fechaDeVenta,
-    Producto producto,
-    int cantidadProducto,
-    string vendedor,
-    string comprador,
-    string tiempoGarantia)
+    public Venta
+    (
+        Guid id,
+        DateTime fechaDeVenta,
+        string nombreProducto,
+        double valorProducto,
+        int cantidadProducto,
+        string vendedor,
+        string comprador,
+        string tiempoGarantia
+    )
 
     {
         Id = id;
         FechaDeVenta = fechaDeVenta;
-        Producto = producto;
+        NombreProducto = nombreProducto;
+        ValorProducto = valorProducto;
         CantidadProducto = cantidadProducto;
         Vendedor = vendedor;
         Comprador = comprador;
@@ -61,19 +60,3 @@ public class Venta
     }
 
 }
-
-public class Producto
-{
-    Guid Id { set; get; }
-    string Nombre { set; get; }
-    double Valor { set; get; }
-
-    public Producto(Guid id, string nombre, double valor)
-    {
-        Id = id;
-        Nombre = nombre;
-        Valor = valor;
-    }
-}
-
-
