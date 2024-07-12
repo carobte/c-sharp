11b62718f338f88289bb2bb88cff646c55a247de
carobte <caro.bustamante.escobar@gmail.com>
2024-07-12 08:50:23 -0500

    feat: Menu, mostrar ventas y promedio

--- Entregables_LINQ/Tienda/Program.cs
+++ Entregables_LINQ/Tienda/Program.cs
@@ -3,7 +3,7 @@
 void CrearVenta()
 {
     var id = Guid.NewGuid();
-    var fecha = new DateTime();
+    var fecha = DateTime.Now;
 
     Console.WriteLine("Ingresa el nombre del producto: ");
     var nombre = Console.ReadLine();
@@ -23,18 +23,98 @@ void CrearVenta()
     Console.WriteLine("Ingresa el tiempo de garantía: ");
     var garantia = Console.ReadLine();
 
+    listaVentas.Add(new Venta(id, fecha, nombre, valor, cantidad, vendedor, comprador, garantia));
 }
+
+void MostrarVentas()
+{
+    foreach (Venta venta in listaVentas)
+    {
+        Console.WriteLine($"================= Factura ==================");
+        Console.WriteLine($"ID:                     {venta.Id}");
+        Console.WriteLine($"Fecha de Venta:         {venta.FechaDeVenta}");
+        Console.WriteLine($"Vendedor:               {venta.Vendedor}");
+        Console.WriteLine($"Comprador:              {venta.Comprador}");
+        Console.WriteLine($"Valor del Producto:     {venta.ValorProducto:C}"); // :C formatea como moneda
+        Console.WriteLine($"Cantidad:               {venta.CantidadProducto}");
+        Console.WriteLine($"Tiempo de Garantía:     {venta.TiempoGarantia}");
+        Console.WriteLine($"Total Facturado:        {venta.ValorProducto * venta.CantidadProducto:C} COP");
+        Console.WriteLine("===========================================\n");
+    }
+}
+
+void PromedioVentas()
+{
+    Console.WriteLine("Escribe la fecha en el formato día/mes/año del promedio que requieres");
+    var fecha = Convert.ToDateTime(Console.ReadLine());
+
+    var listaTotales = listaVentas.Where(venta => venta.FechaDeVenta.Date == fecha.Date) // Filtramos las compras de la fecha ingresada por el usuario
+    .Select(venta => venta.ValorProducto * venta.CantidadProducto) // Seleccionamos el valor * cantidad (total de cada venta)
+    .ToList(); // Convertimos en lista para poder usar sus métodos
+
+    // Si la lista, tiene 0 elementos es porque no hay ventas ese día
+   if(listaTotales.Count == 0)
+    {
+       Console.WriteLine("El día ingresado no se encontraron ventas registradas.");
+    } else  // sino, calculamos el promedio con el método Average
+    {
+        var promedio = listaTotales.Average();
+        Console.WriteLine($"El promedio de compras de {fecha} fue de {promedio:C} COP");
+    }  
+
+}
+
+
+void Menu()
+{
+    bool continuar = true;
+    while (continuar)
+    {
+        Console.WriteLine("===========================================");
+        Console.WriteLine("          Menú de Gestión de Ventas         ");
+        Console.WriteLine("===========================================");
+        Console.WriteLine("1 Ingresar venta");
+        Console.WriteLine("2 Ver promedio ventas día específico");
+        Console.WriteLine("3 Imprimir todas las facturas");
+        Console.WriteLine("4 Cerrar");
+        Console.WriteLine("===========================================");
+        Console.Write("Selecciona una opción: ");
+
+        string opcion = Console.ReadLine();
+        switch (opcion)
+        {
+            case "1":
+                CrearVenta();
+                break;
+            case "2":
+                PromedioVentas();
+                break;
+            case "3":
+                MostrarVentas();
+                break;
+            case "4":
+                Console.WriteLine("Hasta luego, vuelva pronto");
+                continuar = false;
+                break;
+            default:
+
+                break;
+        }
+    }
+}
+
+Menu();
 
 public class Venta
 {
-    Guid Id { set; get; }
-    DateTime FechaDeVenta { set; get; }
-    string NombreProducto { set; get; }
-    double ValorProducto { set; get; }
-    int CantidadProducto { set; get; }
-    string Vendedor { set; get; }
-    string Comprador { set; get; }
-    string TiempoGarantia { set; get; }
+    public Guid Id { set; get; }
+    public DateTime FechaDeVenta { set; get; }
+    public string NombreProducto { set; get; }
+    public double ValorProducto { set; get; }
+    public int CantidadProducto { set; get; }
+    public string Vendedor { set; get; }
+    public string Comprador { set; get; }
+    public string TiempoGarantia { set; get; }
 
     public Venta
     (
