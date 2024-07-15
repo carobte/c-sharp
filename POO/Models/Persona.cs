ac8bbbdcdc18f0ff3d267e57c8834be19513a4f5
carobte <caro.bustamante.escobar@gmail.com>
2024-07-15 08:11:16 -0500

    feat: ejemplo

--- POO/Models/Persona.cs
+++ POO/Models/Persona.cs
@@ -1,7 +1,7 @@
 // using es similar al import de JS
 // Aunque estén desactivadas, no se recomienda borrarlas por el editor
 
-using System; 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,18 +14,30 @@ public class Persona
 {
     public int Id { get; set; }
     public string? Nombre { get; set; }
-    public DateOnly FechaNacimiento { get; set; }
+    // public DateOnly FechaNacimiento { get; set; }
     public string? Genero { get; set; }
     public string? Nacionalidad { get; set; }
     public string? Telefono { get; set; }
     public string? Correo { get; set; }
 
-
-// encapsulamiento (ej: public), modificador (ej: static) (opcional), retorno (tipo), nombre método (mayus)
-    public void Saludar(){
-        Console.WriteLine($"Hola, mi nombre es {Nombre}");
+    // Método constructor, snippet -> ctor 
+    public Persona(int id, string nombre, string genero, string nacionalidad, string telefono, string correo)
+    {
+        // this es por convención o ayuda visual para el desarrollador
+        this.Id = id;
+        this.Nombre = nombre;
+        // this.FechaNacimiento = fechaNacimiento;
+        this.Genero = genero;
+        this.Nacionalidad = nacionalidad;
+        this.Telefono = telefono;
+        this.Correo = correo;
     }
 
+    // encapsulamiento (ej: public), modificador (ej: static) (opcional), retorno (tipo), nombre método (mayus)
+    public void Saludar()
+    {
+        Console.WriteLine($"Hola, mi nombre es {Nombre}");
+    }
 }
 
 // Diagrama de clases con UML -> Lenguaje Unificado de Modelado
--- POO/Program.cs
+++ POO/Program.cs
@@ -1,5 +1,18 @@
-﻿using POO.Models;
+﻿using POO.Models; // Usamos el namespace para poder utilizar la clase Persona
 
 Console.Clear();
 
-var Coder = new Persona();
+var Coder = new Persona(1, "Caro Bte", "Femenino", "Colombiano", "3123456789", "caro@example.com"); // Instanciamos la clase -> creamos plantilla vacía
+
+/* Podemos llenar los datos así si la clase no tiene constructor:
+
+Coder.Id = 1; 
+Coder.Nombre = "Caro Bte"; 
+Coder.Genero = "Femenino";
+Coder.Nacionalidad = "Colombiano";
+Coder.Telefono = "3123456789";
+Coder.Correo = "caro@example.com";
+
+Sin embargo, es mejor utilizar el método constructor de la clase */
+
+Coder.Saludar();
