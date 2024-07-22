ed96412e4eae0cc4c08e354fbc3ae8e19e4b81de
carobte <caro.bustamante.escobar@gmail.com>
2024-07-22 06:49:01 -0500

    feat: ejemplo de clase

--- POO_Static/Program.cs
+++ POO_Static/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using POO_Static.Models;
+
+// Uso de propiedades y métodos 
+Console.Clear();
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, mi nombre es Caro!");
+
+var ejemplo1 = new Ejemplo();
+Console.WriteLine($"Nombre: {ejemplo1.Nombre}");
+Console.WriteLine($"Apellido: {ejemplo1.Apellido}");
+ejemplo1.Saludar();
+
