71ad9cd516e2229029da808afa5af1692d7da39d
carobte <caro.bustamante.escobar@gmail.com>
2024-07-22 06:43:58 -0500

    feat: se crea la clase ejemplo

---
+++ POO_Static/Models/Ejemplo.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POO_Static.Models
+
+    public class Ejemplo
+    {
+        public string Nombre {get;set;}
+        public string Apellido {get;set;}
+
+        public Ejemplo(){
+            Nombre = "Caro";
+            Apellido = "Bte"
+        }
+
+        public void Saludar()
+        {
+            Console.WriteLine($"Hola! Soy {Nombre} {Apellido}");
+        }
+    }
+
+
+
