b45024f069f61921062c20dcb28742f97e53eb7c
carobte <caro.bustamante.escobar@gmail.com>
2024-07-29 20:12:40 -0500

    fix: carpeta POO escuela

---
+++ POO_Polimorfismo/Program.cs
@@ -0,0 +1,19 @@
+﻿using POO_Polimorfismo.Models;
+
+Console.Clear();
+
+// var perroNuevo = new Perro();
+// var gatoNuevo = new Gato();
+// var pezNuevo = new Pez();
+
+// //Llamamos el método Hablar
+// perroNuevo.Hablar(); //EL animal esta hablando y después El perro dice wauff!
+// gatoNuevo.Hablar(); //EL animal esta hablando y después El gato dice meow!
+// pezNuevo.Hablar();
+
+// //Llamamos el método Desplazarse
+// perroNuevo.Desplazarse();
+// gatoNuevo.Desplazarse();
+// pezNuevo.Desplazarse();
+
+var miPrimerGato = new Gato();
